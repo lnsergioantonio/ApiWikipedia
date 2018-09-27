f8b9edca1e9a4fa3d30fc6022888c1d3476563fe
sergio <lnsergioantonio@gmail.com>
2018-09-27 12:59:10 -0500

    init

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var btnSearch: UIButton!
+    @IBOutlet weak var lbMessage: UILabel!
+    var word: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        btnSearch.isEnabled = false
+        lbMessage.text = ""
     }
+    @IBAction func textFieldChanged(_ sender: Any) {
+        if !textField.text!.isEmpty{
+            btnSearch.isEnabled=true
+        }else{
+            btnSearch.isEnabled=false
+        }
+        self.lbMessage.text = ""
+    }
+    
 
-
+    @IBAction func searchWord(_ sender: Any) {
+        if !textField.text!.isEmpty{
+            word = textField.text!
+            
+            let preparatedWord = word?.replacingOccurrences(of: " ", with: "%20")
+            let urlApiWiki = URL(string: "https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=\(preparatedWord!)")
+            let task = URLSession.shared.dataTask(with: urlApiWiki!){(data,response,error) in
+                    if error != nil{
+                        print(error!)
+                    }else{
+                        do{
+                            let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:Any]
+                            let query = json["query"] as! [String:Any]
+                            let pages = query["pages"] as! [String:Any]
+                            let pageKeys = pages.keys
+                            let pageId = pageKeys.first!
+                            if pageId != "-1" {
+                                let extract = pages[pageId] as! [String:Any]
+                                let htmlSource = extract["extract"] as! String
+                                DispatchQueue.main.sync(execute: {
+                                    self.webView.loadHTMLString(htmlSource, baseURL: nil)
+                                })
+                            }else{
+                                DispatchQueue.main.sync(execute: {
+                                    self.lbMessage.text = "Palabra no encontrada - Pruebe intentando de manera diferente"
+                                })
+                            }
+                        }catch{
+                            print("error al leer el json")
+                        }
+                    }
+                }
+            task.resume()
+        }
+    }
 }
 
